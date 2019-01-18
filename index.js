ff6181f59487fbead72774aafb818c60d415ccbc
AlexVotry <alexvotry@gmail.com>
2019-01-17 16:13:35 -0800

    set up Oauth and mLab

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const thirtyDays = 30 * 24 * 60 * 60 * 1000;
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 // const apiRoutes = require('./routes/apiRoutes');
 
-app.use(express.urlencoded({ extended: true}));
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(
+  cookieSession({
+    MaxAge: thirtyDays,
+    keys: [keys.cookieKey]
+  })
+);
+// create global req.user.
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
